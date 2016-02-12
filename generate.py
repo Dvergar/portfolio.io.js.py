93e06b822808b25037df5c236f26ba735f52c803
Dvergar <laurent.rd@gmail.com>
2016-02-12 11:57:16 +0100

    first commit

---
+++ generate.py
@@ -0,0 +1,23 @@
+import os
+
+import yaml
+from jinja2 import Template, Environment, FileSystemLoader
+
+
+datas_types = {}
+for f in os.listdir("datas"):
+
+    file_name = os.path.splitext(f)[0]
+    with open(os.path.join('datas', f), 'r') as stream:
+	    datas = yaml.load(stream)
+    datas_types[file_name] = datas
+
+
+env = Environment(loader=FileSystemLoader(""))
+tpl = env.get_template("template.html")
+output = tpl.render(
+    datas_types=datas_types)
+
+path = os.path.join("index.html")
+with open(path, "wb") as fh:
+    fh.write(output.encode('utf-8'))

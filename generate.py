3fa06dc5dbc7833143c1d875aaabd535b4f97472
Dvergar <laurent.rd@gmail.com>
2016-02-14 12:07:49 +0100

    FIXED relative directory issues

--- generate.py
+++ generate.py
@@ -17,6 +17,9 @@ arguments = docopt(__doc__, argv=None, help=True, version=None, options_first=Fa
 print arguments
 
 
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
 if arguments['makeproject']:
     name = arguments['<name>']
     path = arguments['<path>']
@@ -28,41 +31,51 @@ if arguments['makeproject']:
     if not os.path.exists(path):
         os.makedirs(path)
 
-    # MAKE DEFAULT PROJECT FROM SAPLE FILES
-    shutil.copy(os.path.join('sample','settings.yaml'), path)
-    shutil.copy(os.path.join('sample','games.yaml'), path)
+    # MAKE DEFAULT PROJECT FROM SAMPLE FILES
+    shutil.copy(os.path.join(SCRIPT_PATH, 'sample','settings.yaml'), path)
+    shutil.copy(os.path.join(SCRIPT_PATH, 'sample','games.yaml'), path)
 
 
 if arguments['build']:
-    path = arguments['<path>']
+    FILE_PATH = arguments['<path>']
+    PATH, _ = os.path.split(FILE_PATH)
+    ABS_PATH = os.path.join(SCRIPT_PATH, PATH)
 
+    # LOAD USER PROJECT FILES
     datas_types = {}
-    for f in os.listdir(path):
+    for f in os.listdir(FILE_PATH):
         file_name = os.path.splitext(f)[0]
         file_ext = os.path.splitext(f)[-1]
 
-        if not os.path.isfile(os.path.join(path, f)):
+        # FILES TO IGNORE
+        if not os.path.isfile(os.path.join(FILE_PATH, f)):
             continue
         if not file_ext == '.yaml':
             continue
 
+        # SETTING FILE
         if file_name == 'settings':
-            with open(os.path.join(path, f), 'r') as stream:
+            with open(os.path.join(FILE_PATH, f), 'r') as stream:
                 settings = yaml.load(stream)
             continue
 
-        with open(os.path.join(path, f), 'r') as stream:
+        # ITEM FILES
+        with open(os.path.join(FILE_PATH, f), 'r') as stream:
             datas_types[file_name] = yaml.load(stream)
 
-    env = Environment(loader=FileSystemLoader(""))
+
+    # CREATE EXPORT PATH
+    EXPORT_PATH = os.path.join(FILE_PATH, 'export')
+    if not os.path.exists(EXPORT_PATH):
+        os.mkdir(EXPORT_PATH)
+        copy_tree(os.path.join(SCRIPT_PATH, "datas"), os.path.join(EXPORT_PATH, "datas"))
+
+    # RENDER TEMPLATE
+    env = Environment(loader=FileSystemLoader(ABS_PATH))
     tpl = env.get_template("template.html")
     output = tpl.render(
         datas_types=datas_types, settings=settings)
 
-    export_path = os.path.join(path, 'export')
-    if not os.path.exists(export_path):
-        os.mkdir(export_path)
-        copy_tree("datas", os.path.join(export_path, "datas"))
-
-    with open(os.path.join(export_path, "index.html"), "wb") as fh:
+    # GENERATE INDEX.HTML
+    with open(os.path.join(EXPORT_PATH, "index.html"), "wb") as fh:
         fh.write(output.encode('utf-8'))

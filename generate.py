ec0446f6147fc9a0f90de5e136771eec6203a670
Dvergar <laurent.rd@gmail.com>
2016-02-14 22:55:10 +0100

    smart color generation for palette.yaml
    Automatically fill the palette from missing
    set colors

--- generate.py
+++ generate.py
@@ -16,6 +16,8 @@ from distutils.dir_util import copy_tree
 import shutil
 
 import yaml
+from colour import Color
+import spectra
 import markdown
 from docopt import docopt
 from jinja2 import Template, Environment, FileSystemLoader
@@ -86,6 +88,46 @@ if arguments['build']:
     # LOAD PALETTE
     with open(os.path.join(SCRIPT_PATH, "palette.yaml"), 'r') as stream:
         palette = yaml.load(stream)
+
+    box_background_color = palette.get('box_background')
+    if box_background_color is None:
+        # GENERATE box_background
+        color = spectra.html(palette['website_background']).to("hsv")
+        hsl = list(color.values)
+        hsl[2] = 0.25
+        color = spectra.hsl(*hsl)
+        palette['box_background'] = color.hexcode
+
+    headers_color = palette.get('headers')
+    if headers_color is None:
+        # GENERATE headers
+        color = spectra.html(palette['website_background']).to("hsv")
+        hsl = list(color.values)
+        hsl[2] = 0.23
+        color = spectra.hsl(*hsl)
+        palette['headers'] = color.hexcode
+
+    box_headers_color = palette.get('box_headers')
+    if box_headers_color is None:
+        # GENERATE box_headers
+        color = spectra.html(palette['box_background']).to("lab")
+        lab = list(color.values)
+        lab[0] = 51
+        color = spectra.lab(*lab)
+        palette['box_headers'] = color.hexcode
+
+        # GENERATE box_description
+        color = spectra.html(palette['box_background']).to("lab")
+        lab = list(color.values)
+        lab[0] = 0
+        color = spectra.lab(*lab)
+        palette['box_description'] = color.hexcode
+
+    labels_color = palette.get('labels')
+    if labels_color is None:
+        # GENERATE labels
+        palette['labels'] = palette['box_headers']
+
 
     # CREATE EXPORT PATH
     EXPORT_PATH = os.path.join(FILE_PATH, 'export')
